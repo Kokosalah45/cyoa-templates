8a7c950cd96c3cc0b40b6eb86140aa7fd61286d7
Kerolous Amged <Kokp.amged@gmail.com>
2024-04-10 08:18:33 +0200

    feat:css and handling path params

--- main.go
+++ main.go
@@ -1,39 +1,56 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"text/template"
+	"strings"
 )
+
+func initStaticPath(mux *http.ServeMux) {
+	fs := http.FileServer(http.Dir("./public"))
+	mux.Handle("/public/", fs)
+
+}
 
 func main() {
 
-	dirs := []string{
+
+	mux := http.NewServeMux()
+
+	initStaticPath(mux)
+
+	templateDirs := []string{
 		"*.html",
 		"partials/*.html",
 	}
 
-	tmpl, err := template.ParseFS(os.DirFS("views"), dirs...)
-
+	tmpl, err := template.ParseFS(os.DirFS("views"), templateDirs...)
+	
 	if err != nil {
 		panic(err)
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		tmpl.ExecuteTemplate(w, "index", "")
-	})
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.ExecuteTemplate(w, "about", struct{
-			X string
-			Y string
-		}{
-			X : "kero",
-			Y : "mero",
-		})
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		params := strings.Split(r.URL.Path[1:], "/")
+		if r.Method == "GET" {
+			if len(params) > 0  {
+				if(!(params[0] == "favicon.ico" || params[0] == "")){
+					fmt.Fprintf(w, "Hello %s", params[0])
+					return
+				}
+			}
+
+			tmpl.ExecuteTemplate(w, "index", "")
+
+		}
+
 	})
 
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", mux))
 
 }
